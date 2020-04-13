de64ad1aa89050534a434b753c844427507e21c2
tacketar <alan@atackett.com>
2020-04-13 12:00:39 -0500

    Extend direct_io to handle non-seekable and streaming fds

--- src/toolbox/direct_io.c
+++ src/toolbox/direct_io.c
@@ -14,6 +14,9 @@
    limitations under the License.
 */
 
+
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <errno.h>
@@ -61,22 +64,25 @@ void tbx_dio_finish(FILE *fd, int flags)
 ssize_t tbx_dio_read(FILE *fd, char *buf, ssize_t nbytes, ssize_t offset)
 {
     ssize_t n, ntotal, nleft;
-    int nfd, flags, old_flags;
+    int nfd, flags, old_flags, is_seekable;
+    struct stat sbuf;
 
     nfd = fileno(fd);
+    fstat(nfd, &sbuf);
+    is_seekable = (((sbuf.st_mode & S_IFREG) > 0) && (offset != -1)) ? 1 : 0;
 
     ntotal = 0;
     nleft = nbytes;
     do {
         //** Try and do the I/O with direct I/O
-        n = pread(nfd, buf + ntotal, nleft, offset + ntotal);
+        n = (is_seekable) ? pread(nfd, buf + ntotal, nleft, offset + ntotal) : read(nfd, buf + ntotal, nleft);
         log_printf(5, "nfd=%d nleft=" SST " got=" SST " ntotal=" SST " lseek=" SST " offset=" SST " errno=%d\n", nfd, nleft, n, ntotal, lseek(nfd, 0L, SEEK_CUR), offset, errno);
         if (n == -1) {  //** Got an error so try without Direct I/O
             old_flags = fcntl(nfd, F_GETFL);
             flags = old_flags ^ O_DIRECT;
             fcntl(nfd, F_SETFL, flags);
 
-            n = pread(nfd, buf + ntotal, nleft, offset + ntotal);
+            n = (is_seekable) ? pread(nfd, buf + ntotal, nleft, offset + ntotal) : read(nfd, buf + ntotal, nleft);
             log_printf(5, "RETRY nfd=%d nleft=" SST " got=" SST " ntotal=" SST " lseek=" SST " offset=" SST " errno=%d\n", nfd, nleft, n, ntotal, lseek(nfd, 0L, SEEK_CUR), offset, errno);
 
             fcntl(nfd, F_SETFL, old_flags);
@@ -102,22 +108,25 @@ ssize_t tbx_dio_read(FILE *fd, char *buf, ssize_t nbytes, ssize_t offset)
 ssize_t tbx_dio_write(FILE *fd, char *buf, ssize_t nbytes, ssize_t offset)
 {
     ssize_t n, ntotal, nleft;
-    int nfd, flags, old_flags;
+    int nfd, flags, old_flags, is_seekable;
+    struct stat sbuf;
 
     nfd = fileno(fd);
+    fstat(nfd, &sbuf);
+    is_seekable = (((sbuf.st_mode & S_IFREG) > 0) && (offset != -1)) ? 1 : 0;
 
     ntotal = 0;
     nleft = nbytes;
     do {
         //** Try and do the I/O with direct I/O
-        n = pwrite(nfd, buf + ntotal, nleft, offset + ntotal);
+        n = (is_seekable) ? pwrite(nfd, buf + ntotal, nleft, offset + ntotal) : write(nfd, buf + ntotal, nleft);
         log_printf(5, "nfd=%d nleft=" SST " got=" SST " ntotal=" SST " lseek=" SST " offset=" SST " errno=%d\n", nfd, nleft, n, ntotal, lseek(nfd, 0L, SEEK_CUR), offset, errno);
         if (n == -1) {  //** Got an error so try without Direct I/O
             old_flags = fcntl(nfd, F_GETFL);
             flags = old_flags ^ O_DIRECT;
             fcntl(nfd, F_SETFL, flags);
 
-            n = pwrite(nfd, buf + ntotal, nleft, offset + ntotal);
+            n = (is_seekable) ? pwrite(nfd, buf + ntotal, nleft, offset + ntotal) : write(nfd, buf + ntotal, nleft);
             log_printf(5, "RETRY nfd=%d nleft=" SST " got=" SST " ntotal=" SST " lseek=" SST " offset=" SST " errno=%d\n", nfd, nleft, n, ntotal, lseek(nfd, 0L, SEEK_CUR), offset, errno);
 
             fcntl(nfd, F_SETFL, old_flags);
