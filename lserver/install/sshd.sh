d7436406a3b8e06d248c0ee3456dd817f68c0344
tacketar <alan@atackett.com>
2021-03-11 17:07:41 -0600

    Removing placeholder for when docker didn't support 'docker exec -it'.

--- lserver/install/sshd.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-#This line removes the '#'
-sed -i.pkg-v1 -e 's/#Port 22/Port 22/' -e 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
-#and make the substitution
-sed -i.pkg-v2 -e 's/Port 22/Port 2222/' -e 's/PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
