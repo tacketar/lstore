8eb8c4e73ce7c4f88e8a187cdb22d398076be843
tacketar <alan@atackett.com>
2021-10-19 17:43:07 -0500

    Add logging of exnode access since that means they could R/W the data with it.

--- src/lio/os/file.c
+++ src/lio/os/file.c
@@ -3293,6 +3293,11 @@ int osf_get_attr(lio_object_service_fn_t *os, lio_creds_t *creds, osfile_fd_t *o
         return(1);
     }
 
+    //** We log exnode access
+    if (strcmp(attr, "system.exnode") == 0) {
+        notify_printf(osf->olog, 1, creds, "ATTR_READ(system.exnode, %d, %s)\n", ofd->ftype, ofd->realpath);
+    }
+
     //** Do a Virtual Attr check
     //** Check the prefix VA's first
     it = tbx_list_iter_search(osf->vattr_prefix, attr, -1);
