cd17bce4778b61f6ec4eb225273bc63c91b56e2c
Alan Tackett <alan@atackett.com>
2024-04-25 15:06:32 -0500

    Code cleanup

--- scripts/copy_libfuse3-lio.sh
+++ scripts/copy_libfuse3-lio.sh
@@ -7,8 +7,6 @@
 
 PREFIX=$1
 
-ls -l ${PREFIX}/build_fuse3*
-
 if [ -e ${PREFIX}/build_fuse3_lio-deb ]; then
     cp -a /usr/lib/x86_64-linux-gnu/libfuse3-lio.so*   /tmp/lstore-package/debian/tmp/usr/lib/x86_64-linux-gnu/
 fi
