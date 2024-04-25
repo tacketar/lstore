6ae1b30e9a340c876ef6785b9d8de510a10dc9d2
Alan Tackett <alan@atackett.com>
2024-04-25 15:07:46 -0500

    Some supported distros don't have FUSE3 yet

--- scripts/generate-docker-base.sh
+++ scripts/generate-docker-base.sh
@@ -111,7 +111,7 @@ for DISTRO in "${DISTROS[@]}"; do
                                     git-core
                                     meson
                                     ninja-build
-                                    libfuse3-dev
+                                    libfuse-dev
                                     libacl1-dev
                                     libapr1-dev
                                     libaprutil1-dev
