37dc373f6edb26f792b23ae04fe8b914e5f69540
Alan Tackett <alan@atackett.com>
2026-02-09 14:02:16 -0600

    Switch top using tbx_append_printf to handle buffer management

--- src/ibp-server/transfer_stats.c
+++ src/ibp-server/transfer_stats.c
@@ -133,7 +133,7 @@ void add_stat(Transfer_stat_t *s)
 int send_stats(tbx_ns_t *ns, ibp_time_t start_time, tbx_ns_timeout_t dt)
 {
     int bufsize = 1024 * 1024;
-    int nmax = bufsize - 200;
+    int nmax = bufsize - 1024;
     char buffer[bufsize];
     char *sptr;
     int i, j, n, nchar, nw, currtime;
@@ -146,23 +146,18 @@ int send_stats(tbx_ns_t *ns, ibp_time_t start_time, tbx_ns_timeout_t dt)
     sptr = buffer;
     nchar = 0;
     currtime = ibp_time_now();
-    n = sprintf(sptr, "%d\n", currtime);
-    sptr = sptr + n;
-    nchar += n;
+    n = tbx_append_printf(sptr, &nchar, bufsize, "%d\n", currtime);
 
     for (i = 0; i < global_config->server.max_threads; i++) {
         ts = global_task[i].stat;
         if (ts.start > 0) {
             if (ts.dir == DIR_IN) {
-                n = snprintf(sptr, sizeof(buffer) - nchar, "%s %s %d %d %d %d 0\n", ts.address,
-                             stats.host, ts.start, currtime, ts.nbytes, ts.id);
+                tbx_append_printf(sptr, &nchar, bufsize,"%s %s %d %d %d %d 0\n", ts.address,
+                                  stats.host, ts.start, currtime, ts.nbytes, ts.id);
             } else {
-                n = snprintf(sptr, sizeof(buffer) - nchar, "%s %s %d %d %d %d 0\n", stats.host,
+                tbx_append_printf(sptr, &nchar, bufsize, "%s %s %d %d %d %d 0\n", stats.host,
                              ts.address, ts.start, currtime, ts.nbytes, ts.id);
             }
-
-            sptr = sptr + n;
-            nchar += n;
         }
     }
 
@@ -175,17 +170,12 @@ int send_stats(tbx_ns_t *ns, ibp_time_t start_time, tbx_ns_timeout_t dt)
                s[i].start);
     while ((s[i].start > start_time) && (j != stats.size)) {
         if (ts.dir == DIR_IN) {
-            n = snprintf(sptr, sizeof(buffer) - nchar, "%s %s %d %d %d %d 1\n", s[i].address,
+            tbx_append_printf(sptr, &nchar, bufsize, "%s %s %d %d %d %d 1\n", s[i].address,
                          stats.host, s[i].start, s[i].end, s[i].nbytes, s[i].id);
         } else {
-            n = snprintf(sptr, sizeof(buffer) - nchar, "%s %s %d %d %d %d 1\n", stats.host,
+            tbx_append_printf(sptr, &nchar, bufsize, "%s %s %d %d %d %d 1\n", stats.host,
                          s[i].address, s[i].start, s[i].end, s[i].nbytes, s[i].id);
         }
-
-//log_printf(15, "send_stats: ns=%d pos=%d  str=%s\n", tbx_ns_getid(ns), i, sptr);
-
-        sptr = sptr + n;
-        nchar += n;
 
         if (nchar > nmax) {
 //log_printf(15, "send_stats: internal ns=%d sending: %s\n", tbx_ns_getid(ns), buffer);
@@ -212,9 +202,7 @@ int send_stats(tbx_ns_t *ns, ibp_time_t start_time, tbx_ns_timeout_t dt)
         j++;
     }
 
-    n = snprintf(sptr, sizeof(buffer) - nchar, "\n");
-    sptr = sptr + n;
-    nchar = nchar + n;
+    tbx_append_printf(sptr, &nchar, bufsize, "\n");
 
     log_printf(15, "send_stats: ns=%d nchar=%d strlen=" ST " sending: %s\n", tbx_ns_getid(ns),
                nchar, strlen(buffer), buffer);
